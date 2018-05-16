ef175a2a05faf8e7090817f9260db805ad26d99a
Diego Araneda <daranedag@gmail.com>
2018-05-16 02:43:57 -0400

    templates agregados y static files agregados

--- HCMFrontDev/salas/views.py
+++ HCMFrontDev/salas/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 def index(request):
-    return HttpResponse("Login inicial")
+	context = {}
+	return render(request, 'salas/index.html', context)
+
+def loginAdmin(request):
+	return HttpResponse("Login para usuario identificado como admin")
+
+def loginEmpleado(request):
+	return HttpResponse("Login para usuario identificado como empleado")
